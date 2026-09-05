5fe5c124c80ef9702a0ea9e7dfb377013bd7a548
Ujjwal <ujjwalrawat248@gmail.com>
2026-09-05 22:24:19 +0530

    Dependency Injection using setter Injection

---
+++ src/main/java/com/dependencyInjection/setterInjection/App.java
@@ -0,0 +1,15 @@
+package com.dependencyInjection.setterInjection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext  =new
+                ClassPathXmlApplicationContext("applicationSetterInjection.xml");
+
+        Car car = (Car) applicationContext.getBean("mycar");
+
+        car.showDetails();
+    }
+}
---
+++ src/main/java/com/dependencyInjection/setterInjection/Specification.java
@@ -0,0 +1,27 @@
+package com.dependencyInjection.setterInjection;
+
+public class Specification {
+    private String brand;
+    private String model;
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    @Override
+    public String toString() {
+        return "Specification { brand : "+brand+", model : "+model+" }";
+    }
+}
