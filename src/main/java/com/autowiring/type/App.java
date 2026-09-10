938d5dbe7983ae28608576ffdf4c57ecdbe4d905
Ujjwal <ujjwalrawat248@gmail.com>
2026-09-10 18:50:56 +0530

    Autowire by type

---
+++ src/main/java/com/autowiring/type/App.java
@@ -0,0 +1,18 @@
+package com.autowiring.type;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext  =new
+                ClassPathXmlApplicationContext("autowireByType.xml");
+
+        Car car = (Car) applicationContext.getBean("mycar");
+
+        // so now if you created multiple beans of same type in this case Specification then it can cause confusion and cause errors
+        // so we need to remove other beans from the xml file .
+
+        car.showDetails();
+    }
+}
---
+++ src/main/java/com/autowiring/type/Car.java
@@ -0,0 +1,23 @@
+package com.autowiring.type;
+
+public class Car {
+    private Specification specification;
+
+    public void setSpecification(Specification specification) {
+        this.specification = specification;
+    }
+
+    public Car(){
+        // since we have parmetrized constructor below but autoiwre by name will use setter to inject the dependency and will throw error if we don't have any default constructor.
+        // If you want Autowiring by name to work then name of the bean and name of the property should be same so that dependency can be injected based on the name
+        // so we need to create a bean with name as specification in the xml file .
+    }
+
+    public Car(Specification specification) {
+        this.specification = specification;
+    }
+
+    public void showDetails(){
+        System.out.println("Car Details : "+specification);
+    }
+}
