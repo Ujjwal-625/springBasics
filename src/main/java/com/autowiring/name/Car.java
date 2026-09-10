48fe371b08d56ec32955a39c33cfc3a89bd65fb2
Ujjwal <ujjwalrawat248@gmail.com>
2026-09-10 18:39:08 +0530

    Autowire by Name

--- src/main/java/com/autowiring/name/Car.java
+++ src/main/java/com/autowiring/name/Car.java
@@ -9,7 +9,8 @@ public class Car {
 
     public Car(){
         // since we have parmetrized constructor below but autoiwre by name will use setter to inject the dependency and will throw error if we don't have any default constructor.
-
+        // If you want Autowiring by name to work then name of the bean and name of the property should be same so that dependency can be injected based on the name
+        // so we need to create a bean with name as specification in the xml file .
     }
 
     public Car(Specification specification) {
